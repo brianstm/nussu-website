13d82995f17d27b07024c41d5841e20b7ec53833
Arnav Goel <arnav.goel1@outlook.com>
2025-02-26 21:33:35 +0800

    added scroll indicator

---
+++ src/components/ui/ScrollIndicator.tsx
@@ -0,0 +1,22 @@
+import { motion } from "framer-motion";
+
+export default function ScrollIndicator() {
+  return (
+    <div className="flex flex-col items-center space-y-2">
+      <div className="relative h-8 w-[2px] bg-gray-800 overflow-hidden">
+        <motion.div
+          className="absolute left-0 top-0 h-3 w-full bg-[#EF7B00]"
+          initial={{ y: 0 }}
+          animate={{ y: [0, 20, 0] }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+        />
+      </div>
+
+      <p className="tracking-widest text-[#808080]">SCROLL</p>
+    </div>
+  );
+}
--- src/pages/CampusLife.tsx
+++ src/pages/CampusLife.tsx
@@ -1,14 +1,16 @@
 // Arnav
 
+import ScrollIndicator from "@/components/ui/ScrollIndicator.tsx";
+
 function CampusLife() {
-    return (
-      <div>
-        <h1>CampusLife</h1>
-        <p>This is the CampusLife Page</p>
-        <p>To be done by: Arnav</p>
-      </div>
-    );
-  }
-  
-  export default CampusLife;
-  
+  return (
+    <div>
+      <h1>CampusLife</h1>
+      <p>This is the CampusLife Page</p>
+      <p>To be done by: Arnav</p>
+      <ScrollIndicator />
+    </div>
+  );
+}
+
+export default CampusLife;
